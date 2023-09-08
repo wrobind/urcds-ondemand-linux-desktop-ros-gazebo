48a6c502c7a1b54bdab0fac1284aa83752616a8b
Robin Webber <robin_webber@harvard.edu>
2023-09-07 20:47:34 -0400

    works idempotently

--- extra/class_setup.sh
+++ extra/class_setup.sh
@@ -6,30 +6,31 @@
 
 PATH=/bin:/usr/bin
 
-export SEMESTER=FALL2023
+SEMESTER=FALL2023
 ROS_DISTRO=noetic
 ROS_ENV_SOURCE=/opt/ros/${ROS_DISTRO}/setup.bash
 
-export SHARED_DATA=/n/holyscratch01/Academic-cluster/${SEMESTER}/$(id -g -n)/SHARED
+SHARED_DATA=/n/holyscratch01/Academic-cluster/${SEMESTER}/$(id -g -n)/SHARED
 # Add symlink to a shared data folder containing datasets
 if [ -e "$SHARED_DATA" ]; then
-    if [ ! -e "$HOME/shared_data" ]; then
+    if  [ ! -e "$HOME/shared_data" ]; then
         ln -s $SHARED_DATA $HOME/shared_data
     fi
 else
-    echo "Could not find your class shared_data folder at ${SHARED_DATA} and cannot proceed." 1>&2
+    echo "Could not find your class shared_data folder at ${SHARED_DATA} and cannot proceed with robotics environment setup." 1>&2
     exit 1
 fi
 
-export SCRATCH_PATH=/n/holyscratch01/Academic-cluster/${SEMESTER}/$(id -g -n)/SCRATCH
-export MYSCRATCH=${SCRATCH_PATH}/$USER
+SCRATCH_PATH=/n/holyscratch01/Academic-cluster/${SEMESTER}/$(id -g -n)/SCRATCH
+MYSCRATCH=${SCRATCH_PATH}/$USER
+
 if [ -e ${SCRATCH_PATH} -a -w ${SCRATCH_PATH} -a -d ${SCRATCH_PATH} ]; then
     # create the folder if it does not exists
     if [ ! -e "$MYSCRATCH" ]; then
         mkdir -m 700 $MYSCRATCH
     fi
 else
-    echo "Could not create your personal scratch folder and cannot proceed." 1>&2
+    echo "Could not create your personal scratch folder and cannot proceed with robotics environment setup." 1>&2
     exit 1
 fi
 
@@ -41,19 +42,19 @@ fi
 # update ~/.bashrc so all new shells are ROS-ready with class-local packages
 if [ -e ${ROS_ENV_SOURCE} -a -r ${ROS_ENV_SOURCE} ]; then
 
+    grep -F "${ROS_ENV_SOURCE}" ~/.bashrc ||
+    echo "source ${ROS_ENV_SOURCE}" >> ~/.bashrc
+
     source ${ROS_ENV_SOURCE}
 
-    if [ ! -s "$(grep -F /opt/ros/${ROS_DISTRO}/setup.bash ~/.bashrc 2> /dev/null | grep source)" ]; then
-        echo "source /opt/ros/${ROS_DISTRO}/setup.bash" >> ~/.bashrc
-    fi
-
-    if [ -s "$(which catkin 2> /dev/null)" ]; then
-        grep -F `catkin locate --shell-verbs` ~/.bashrc ||
-        echo "source $(catkin locate --shell-verbs)" >> ~/.bashrc
-    else
-        echo "Could not locate the catkin command, so setup is incomplete. Please report this."
-	exit 1
-    fi
+#    if [ -s "$(which catkin 2> /dev/null)" ]; then
+#        grep -F `catkin locate --shell-verbs` ~/.bashrc ||
+#        echo "source $(catkin locate --shell-verbs)" >> ~/.bashrc
+#        source $(catkin locate --shell-verbs)
+#    else
+#        echo "Could not locate the catkin command, so setup is incomplete." 1>&2
+#	exit 1
+#    fi
 
     ## leave the defaults in place, Robin W. 2023-09-07
     # grep -F "ROS_IP" ~/.bashrc ||
@@ -63,27 +64,13 @@ if [ -e ${ROS_ENV_SOURCE} -a -r ${ROS_ENV_SOURCE} ]; then
     # echo "export ROS_MASTER_URI=http://\$ROS_IP:11311" >> ~/.bashrc
 
     # with per-class Catkin Workspace in shared_data
-    if [ -e ~/shared_data/catkin_ws/devel -a -r ~/shared_data/catkin_ws/devel/setup.bash ]; then
+    if [ -d ~/shared_data/catkin_ws/devel -a -r ~/shared_data/catkin_ws/devel/setup.bash ]; then
+        grep -F 'source ~/shared_data/catkin_ws/devel/setup.bash' ~/.bashrc ||
         echo "source ~/shared_data/catkin_ws/devel/setup.bash" >> ~/.bashrc
         source ~/shared_data/catkin_ws/devel/setup.bash
     else
         echo "The Catkin Workspace for the class could not be found, and has not been set up." 1>&2
     fi
-
-    if [ -s "$(which rosdep 2> /dev/null)" ]; then
-        # users cannot 'sudo rosdep init' so assume that's done and just update local cache
-        rosdep update 2>&1 > /dev/null
-    else
-        echo "Could not find rosdep command! Installation is incomplete. Please report this." 1>&2
-    fi
-
-    echo ""
-    echo "Completed setting up the class, unless there was an error message."
-    echo "Please contact a teaching assistant about any error messages or if you have further questions."
-    echo "If there were no problems reported from the last command,"
-    echo "Please run 'source ~/.bashrc' in this window."
-    echo ""
-
 else
     echo "Could not find ROS ${ROS_DISTRO} setup.bash file. Is ROS insalled on this computer?" 1>&2
 fi
